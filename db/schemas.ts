970ff5b22283ef66b5c9057a5ff6d839c19b5005
Tuan Le <leanhtuan030523@gmail.com>
2024-06-25 19:42:55 +0700

    Add address column to user table

--- db/schemas.ts
+++ db/schemas.ts
@@ -1,9 +1,10 @@
-import { geometry, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { geometry, pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const User = pgTable("account", {
     id: uuid("id").primaryKey().defaultRandom(),
     name: varchar("name", { length: 255 }).notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
     password: varchar("password").notNull(),
-    location: geometry("location", {type: "point"}).notNull()
+    location: geometry("location", { type: "point" }).notNull(),
+    address: text("address")
 })
