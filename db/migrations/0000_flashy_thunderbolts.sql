ce2c744adcb14494b6e40b75f67c99e703f6e09e
Tuan Le <leanhtuan030523@gmail.com>
2024-06-25 19:38:49 +0700

    Remove

--- db/migrations/0000_flashy_thunderbolts.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "account" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"password" varchar NOT NULL,
-	"location" geometry(point) NOT NULL,
-	CONSTRAINT "account_email_unique" UNIQUE("email")
-);
