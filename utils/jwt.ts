3bbaa225d14aad32da93ed334d08812e18060edf
Tuan Le <leanhtuan030523@gmail.com>
2024-06-26 19:18:40 +0700

    Create verifyJWT and generateJWT function

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,3 +1,5 @@
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
+
 export function getSecretKey():Uint8Array{
     const SECRET_KEY = process.env.JWT_SECRET_KEY;
     if (!SECRET_KEY) {
@@ -5,4 +7,27 @@ export function getSecretKey():Uint8Array{
     }
 
     return new TextEncoder().encode(SECRET_KEY);
+}
+
+export async function verifyJWT(token: string):Promise<JWTPayload|null> {
+    try {
+        const { payload } = await jwtVerify(token, getSecretKey());
+        return payload
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function generateJWTToken(
+    name: string,
+    id: string,
+    duration: string | number | Date
+) {
+    return await new SignJWT({
+        username: name,
+        id: id
+    }).setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime(duration)
+        .sign(getSecretKey())
 }
