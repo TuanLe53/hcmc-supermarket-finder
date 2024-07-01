44e7fb46e652282ba7183d7b529358b22ef44e1f
Tuan Le <leanhtuan030523@gmail.com>
2024-07-01 19:25:47 +0700

    Create map page

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { Coordinate } from "@/types";
+import { Coordinate, LocationMarker } from "@/types";
 import { useState } from "react";
 
 import dynamic from "next/dynamic";
+import ControlPanel from "@/components/controlPanel";
 
 const Map = dynamic(() => import("@/components/map"), {
 	ssr: false,
 	loading: () => <p>Loading...</p>
 })
 
-
-
 function MapPage() {
 	const [selectedLocation, setSelectedLocation] = useState<Coordinate | null>(null);
+	const [markers, setMarkers] = useState<LocationMarker[]>([]);
+	const [markerType, setMarkerType] = useState<"wishlist" | "supermarket" | "detail">("supermarket");
+
+	const [flyToLocation, setFlyToLocation] = useState<[number, number] | null>(null);
 
 	return (
 		<main className="h-screen w-full grid grid-cols-4">
@@ -21,11 +24,22 @@ function MapPage() {
 				<Map
 					setSelectedLocation={setSelectedLocation}
 					selectedLocation={selectedLocation}
+					markers={markers}
+					markerType={markerType}
+					flyToLocation={flyToLocation}
 				/>
 			</div>
 
 			<div className="bg-orange-300">
-				<p>Hello world</p>
+				<ControlPanel
+					markers={markers}
+					markerType={markerType}
+					setMarkers={setMarkers}
+					setMarkerType={setMarkerType}
+					selectedLocation={selectedLocation}
+					setSelectedLocation={setSelectedLocation}
+					setFlyToLocation={setFlyToLocation}
+				/>
 			</div>
 		</main>
 	)
