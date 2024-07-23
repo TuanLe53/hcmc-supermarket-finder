040364e935af184f378a995993ebb44bb832a5a1
Tuan Le <leanhtuan030523@gmail.com>
2024-07-23 15:05:56 +0700

    add style

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Coordinate } from "@/types";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { FormEvent, useState } from "react";
 
@@ -64,50 +65,80 @@ function RegisterPage() {
     }
 
     return (
-        <main>
-            <h1>Register page</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Username</label>
-                <input
-                    id="name"
-                    type="text"
-                    name="name"
-                    placeholder="Enter your name"
-                    required
-                />
+        <main className="h-screen flex items-center justify-center">
+            <div
+                className="flex flex-col items-center h-4/5 xl:w-2/5 md:w-4/5"
+            >
+                <h1
+                    className="text-center text-6xl font-medium mb-5"
+                >
+                    Register page
+                </h1>
 
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    placeholder="Enter your email"
-                    required
-                />
+                <form
+                    className="flex flex-col w-11/12 p-2 bg-slate-200 rounded-xl"
+                    onSubmit={handleSubmit}
+                >
+                    <label htmlFor="name">Username</label>
+                    <input
+                        id="name"
+                        type="text"
+                        name="name"
+                        placeholder="Enter your name"
+                        required
+                    />
 
-                <label>Address</label>
-                <button onClick={getLocation}>GPS</button>
-                <p>{userLocation === null ? "Enter your location" : `${userLocation.lat}, ${userLocation.long}`}</p>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        required
+                    />
+                    <div>
+                        <label>Location</label>
+                        <p>{userLocation === null ?
+                            <button onClick={getLocation}>Get your location</button>
+                            :
+                            `${userLocation.lat}, ${userLocation.long}`
+                        }
+                        </p>
+                    </div>
 
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    placeholder="Enter your password"
-                    required
-                />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        required
+                    />
 
-                <label htmlFor="password2">Confirm Password</label>
-                <input
-                    id="password2"
-                    type="password"
-                    name="password2"
-                    placeholder="Confirm your password"
-                    required
-                />
-                <button type="submit">Register</button>
-            </form>
+                    <label htmlFor="password2">Confirm Password</label>
+                    <input
+                        id="password2"
+                        type="password"
+                        name="password2"
+                        placeholder="Confirm your password"
+                        required
+                    />
+
+                    <div
+                        className="mt-3 flex flex-row items-center justify-end"
+                    >
+                        <p>
+                            Already have an account? <Link href={"/auth/login"} className="mr-3 text-blue-400">Login</Link>
+                        </p>
+                        <button
+                            type="submit"
+                            className="w-21 p-1 text-xl font-medium rounded-md bg-sky-400 hover:bg-sky-500"
+                        >
+                            Register
+                        </button>
+                    </div>
+                </form>
+            </div>
         </main>
     )
 }
