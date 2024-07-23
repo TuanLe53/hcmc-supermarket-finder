f7b75dd98d678ca65b9ca3a9fa616f01da164763
Tuan Le <leanhtuan030523@gmail.com>
2024-07-23 19:09:10 +0700

    Add table for list of wishlist

--- app/wishlists/page.tsx
+++ app/wishlists/page.tsx
@@ -1,22 +1,45 @@
 import { getUserWishlists } from "@/db/querys/wishlist";
 import { getAuthenticatedUserData } from "@/utils/cookie";
+import { format } from "date-fns";
 import Link from "next/link";
 
 async function WishlistsPage() {
     const userData = await getAuthenticatedUserData();
     const wishlists = await getUserWishlists(userData?.id as string);
 
-    console.log(wishlists);
-
     return (
-        <main>
-            <h1>Wishlists Page</h1>
-            {wishlists.map((wishlist, index) => (
-                <div key={index}>
-                    <Link href={`/wishlists/${wishlist.id}`}>{wishlist.id}</Link>
-                    <p>{wishlist.status}</p>
-                </div>
-            ))}
+        <main
+            className="h-screen mx-24 mt-10"
+        >
+            <h1 className="text-3xl font-medium mb-2">Your Wishlists</h1>
+            <table
+                className="max-h-4/5 border-collapse border border-4 border-slate-500"
+            >
+                <thead>
+                    <tr className="bg-slate-300">
+                        <th className="text-left border border-slate-700">ID</th>
+                        <th className="text-left border border-slate-700">Supermarket</th>
+                        <th className="text-left border border-slate-700">Created at</th>
+                        <th className="text-left border border-slate-700">Updated at</th>
+                        <th className="text-left border border-slate-700">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {wishlists.map((wishlist, index) => (
+                        <tr key={index}>
+                            <td className="py-2 pr-2 text-left border border-slate-700">
+                                <Link href={`/wishlists/${wishlist.id}`}>                                
+                                    {wishlist.id}
+                                </Link>
+                            </td>
+                            <td className="py-2 pr-2 text-left border border-slate-700">{wishlist.supermarket}</td>
+                            <td className="py-2 pr-2 text-left border border-slate-700">{format(wishlist.createdAt as Date, "dd/MM/yyyy")}</td>
+                            <td className="py-2 pr-2 text-left border border-slate-700">{format(wishlist.updatedAt as Date, "dd/MM/yyyy")}</td>
+                            <td className="py-2 pr-2 text-left border border-slate-700">{wishlist.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </main>
     )
 }
