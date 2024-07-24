fd58fbac3444bc3911707eca0f2885d4ce6e1b6e
Tuan Le <leanhtuan030523@gmail.com>
2024-07-24 19:38:16 +0700

    Update

--- app/wishlists/[id]/page.tsx
+++ app/wishlists/[id]/page.tsx
@@ -1,17 +1,27 @@
 import { db } from "@/db/db";
+import { getWishlistItems, isWishlistExists } from "@/db/querys/wishlist";
 import { WishList } from "@/db/schemas";
+import { getAuthenticatedUserData } from "@/utils/cookie";
 import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+import Link from "next/link";
 import { redirect } from "next/navigation";
-import {validate as isUUID} from "uuid";
+import { validate as isUUID } from "uuid";
+
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import InfoIcon from '@mui/icons-material/Info';
+import { format } from "date-fns";
+import { getSupermarketByID } from "@/db/querys/supermarket";
 
 async function WishlistPage({ params }: { params: { id: string } }) {
+    //Check if param is uuid
     if (!isUUID(params.id)) {
         redirect("/error")
     }
 
-    const query = await db.select().from(WishList).where(eq(WishList.id, params.id));
-    if (query.length === 0) {
+    //Check if wishlist exists
+    const wishlist = await isWishlistExists(params.id);
+    if (!wishlist) {
         return (
             <main>
                 <p>Not found</p>
@@ -19,8 +29,15 @@ async function WishlistPage({ params }: { params: { id: string } }) {
         )
     }
 
-    const wishlist = query[0];
+    //Check if req user is wishlist's owner
+    const userData = await getAuthenticatedUserData();
+    if (wishlist.owner !== userData?.id) {
+        redirect("/unauthorized")
+    }
 
+    const supermarket = await getSupermarketByID(wishlist.supermarket);
+    const items = await getWishlistItems(wishlist.id);
+    
     const deleteWishlist = async () => {
         "use server";
         await db.delete(WishList).where(eq(WishList.id, wishlist.id));
@@ -30,14 +47,39 @@ async function WishlistPage({ params }: { params: { id: string } }) {
     }
 
     return (
-        <main>
-            <h1>{wishlist.id}</h1>
-            <p>{wishlist.status}</p>
-            {wishlist.status === "pending" &&
-                <form action={deleteWishlist}>
-                    <button type="submit">DELETE</button>
-                </form>
-            }
+        <main className="relative p-1 h-screen">
+            <div
+                className="absolute"
+            >
+                <Link
+                    href={"/wishlists"}
+                    className="hover:underline"
+                >
+                    <ArrowBackIosNewIcon />
+                    Wishlists
+                </Link>
+            </div>
+            <div
+                className="ml-36 mb-4 w-4/5 border-2 border-red-300"
+            >
+                <h1><InfoIcon />Info</h1>
+                <p>ID: {wishlist.id}</p>
+                <p>Status: {wishlist.status}</p>
+                <p>Created at: {format(wishlist.createAt as Date, "dd/MM/yyyy")}</p>
+                <p>Updated at: {format(wishlist.updatedAt as Date, "dd/MM/yyyy")}</p>
+                {wishlist.status === "pending" &&
+                    <form action={deleteWishlist}>
+                        <button type="submit">DELETE</button>
+                    </form>
+                }
+            </div>
+            <div
+                className="ml-36 w-4/5 border-2 border-red-300"
+            >
+                <h1>Supermarket</h1>
+                <p>{supermarket[0].name}</p>
+                <p>Address: {supermarket[0].address}</p>
+            </div>
         </main>
     )
 }
