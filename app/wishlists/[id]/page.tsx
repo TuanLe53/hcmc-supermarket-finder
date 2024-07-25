882d9849b90f43337a44b688d028aac1ffddff86
Tuan Le <leanhtuan030523@gmail.com>
2024-07-25 19:30:09 +0700

    Add addNewItem to wishlist feature

--- app/wishlists/[id]/page.tsx
+++ app/wishlists/[id]/page.tsx
@@ -1,8 +1,5 @@
-import { db } from "@/db/db";
-import { getWishlistItems, isWishlistExists } from "@/db/querys/wishlist";
-import { WishList } from "@/db/schemas";
+import { addItemToWishlist, deleteWishlistByID, getWishlistItems, isWishlistExists } from "@/db/querys/wishlist";
 import { getAuthenticatedUserData } from "@/utils/cookie";
-import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -10,12 +7,17 @@ import { validate as isUUID } from "uuid";
 import { format } from "date-fns";
 import { getSupermarketByID } from "@/db/querys/supermarket";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import ItemTable from "@/components/itemTable";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import InfoIcon from '@mui/icons-material/Info';
 import StoreIcon from '@mui/icons-material/Store';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
 
 async function WishlistPage({ params }: { params: { id: string } }) {
     //Check if param is uuid
@@ -44,10 +46,21 @@ async function WishlistPage({ params }: { params: { id: string } }) {
     
     const deleteWishlist = async () => {
         "use server";
-        await db.delete(WishList).where(eq(WishList.id, wishlist.id));
+        deleteWishlistByID(wishlist.id);
 
         revalidatePath("/wishlists");
         redirect("/wishlists");
+    }
+
+    const addNewItem = async (formData: FormData) => {
+        "use server";
+
+        const name = formData.get("name");
+        const quantity = formData.get("quantity");
+
+        await addItemToWishlist(wishlist.id, name as string, quantity as string);
+
+        revalidatePath(`/wishlists/${wishlist.id}`)
     }
 
     return (
@@ -79,10 +92,8 @@ async function WishlistPage({ params }: { params: { id: string } }) {
             </div>
 
             <div className="flex flex-row space-x-4 ml-36 w-4/5">
-                <div>
-                    <Collapsible
-                        className="w-1/2 border-2 border-red-300"
-                    >
+                <div className="w-1/2 p-2 h-fit rounded-xl bg-slate-200">
+                    <Collapsible>
                         <div className="flex flex-row justify-between">
                             <h1><StoreIcon />Supermarket</h1>
                             <CollapsibleTrigger>
@@ -96,8 +107,8 @@ async function WishlistPage({ params }: { params: { id: string } }) {
                     </Collapsible>
                 </div>
 
-                <div>
-                    <Collapsible className="w-1/2 border-2 border-red-300">
+                <div className="w-1/2 p-2 h-fit rounded-xl bg-slate-200">
+                    <Collapsible>
                         <div className="flex flex-row justify-between">
                             <h1><AssignmentIcon />Items</h1>
                             <CollapsibleTrigger>
@@ -105,22 +116,56 @@ async function WishlistPage({ params }: { params: { id: string } }) {
                             </CollapsibleTrigger>
                         </div>
                         <CollapsibleContent>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Quantity</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {items.map((item, index) => (
-                                        <tr key={index}>
-                                            <td>{item.name}</td>
-                                            <td>{item.quantity}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                            <ItemTable
+                                items={items}
+                                wishlistID={wishlist.id}
+                                wishlistStatus={wishlist.status}
+                            />
+
+                            {/* Add Item to wishlist */}
+                            <Collapsible className="mt-1">
+                                <div className="flex justify-end">
+                                    <CollapsibleTrigger>
+                                        <button
+                                            className="p-1 rounded-md bg-sky-400 hover:bg-sky-500"
+                                        >
+                                            Add new item
+                                        </button>
+                                    </CollapsibleTrigger>
+                                </div>
+                                <CollapsibleContent>
+                                    <form
+                                        action={addNewItem}
+                                        className="flex flex-row items-end justify-between"
+                                    >
+                                        <div className="flex-grow">
+                                            <Label htmlFor="name">Name</Label>
+                                            <Input
+                                                id="name"
+                                                name="name"
+                                                placeholder="Enter your product's name"
+                                                required
+                                            />
+                                        </div>
+                                        <div className="flex-grow">
+                                            <Label htmlFor="quantity">Quantity</Label>
+                                            <Input
+                                                id="quantity"
+                                                name="quantity"
+                                                placeholder="Please specify the quantity and unit"
+                                                required
+                                            />
+                                        </div>
+                                        <button type="submit">
+                                            <AddCircleIcon
+                                                fontSize="large"
+                                                className="hover:cursor-pointer text-sky-400 hover:text-sky-600"
+                                            />   
+                                        </button>
+                                    </form>
+                                </CollapsibleContent>
+                            </Collapsible>
+
                         </CollapsibleContent>
                     </Collapsible>
                 </div>
