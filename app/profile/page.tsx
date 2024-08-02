e2f49977cd88e5348241442248af243de1a32e27
Tuan Le <leanhtuan030523@gmail.com>
2024-08-02 19:52:00 +0700

    Add user avatar and update page layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,10 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import UpdateDialog from "@/components/updateDialog";
 import { getUserByID, updateUserAddress, updateUsername } from "@/db/querys/user";
 import { getAuthenticatedUserData } from "@/utils/cookie";
-
 import { revalidatePath } from "next/cache";
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default async function ProfilePage(){
     const userFromToken = await getAuthenticatedUserData();
@@ -22,9 +24,16 @@ export default async function ProfilePage(){
     }
 
     return (
-        <main>
-            <h1>Profile Page</h1>
-            <div>
+        <main
+            className="h-screen grid grid-rows-3 grid-flow-col gap-4"
+        >
+            <div className="row-span-3 bg-red-400">
+                <Avatar>
+                    <AvatarImage src="" />
+                    <AvatarFallback className="bg-transparent">
+                        <AccountCircleIcon fontSize="large" />
+                    </AvatarFallback>
+                </Avatar>
                 <div className="flex flex-row">
                     <p>{user?.name}</p>
                     <UpdateDialog updateField={"username"} action={updateName}/>
@@ -33,6 +42,12 @@ export default async function ProfilePage(){
                     <p>Address: {user?.address}</p>
                     <UpdateDialog updateField={"address"} action={updateAddress}/>
                 </div>
+            </div>
+            <div className="col-span-2 bg-sky-400">
+                <p>wishlist</p>
+            </div>
+            <div className="col-span-2 bg-red-600">
+                <p>trip</p>
             </div>
         </main>
     )
