f1f4935b3345fd351d0b113ca5543584d1f13b1a
Tuan Le <leanhtuan030523@gmail.com>
2024-08-02 19:22:29 +0700

    Add update username dialog

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,5 @@
 import UpdateDialog from "@/components/updateDialog";
-import { getUserByID, updateUserAddress } from "@/db/querys/user";
+import { getUserByID, updateUserAddress, updateUsername } from "@/db/querys/user";
 import { getAuthenticatedUserData } from "@/utils/cookie";
 
 import { revalidatePath } from "next/cache";
@@ -15,14 +15,23 @@ export default async function ProfilePage(){
         revalidatePath("/profile")
     }
 
+    async function updateName(newName: string) {
+        "use server";
+        await updateUsername(user?.id as string, newName)
+        revalidatePath("/profile");
+    }
+
     return (
         <main>
             <h1>Profile Page</h1>
             <div>
-                <p>{user?.name}</p>
+                <div className="flex flex-row">
+                    <p>{user?.name}</p>
+                    <UpdateDialog updateField={"username"} action={updateName}/>
+                </div>
                 <div className="flex flex-row">
                     <p>Address: {user?.address}</p>
-                    <UpdateDialog action={updateAddress}/>
+                    <UpdateDialog updateField={"address"} action={updateAddress}/>
                 </div>
             </div>
         </main>
