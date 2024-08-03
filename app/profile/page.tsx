85437534324c1fecc7977a717fc0566c4e7a0c57
Tuan Le <leanhtuan030523@gmail.com>
2024-08-03 20:11:39 +0700

    Add style to UserInfo section

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -27,27 +27,83 @@ export default async function ProfilePage(){
         <main
             className="h-screen grid grid-rows-3 grid-flow-col gap-4"
         >
-            <div className="row-span-3 bg-red-400">
-                <Avatar>
-                    <AvatarImage src="" />
-                    <AvatarFallback className="bg-transparent">
-                        <AccountCircleIcon fontSize="large" />
-                    </AvatarFallback>
-                </Avatar>
-                <div className="flex flex-row">
-                    <p>{user?.name}</p>
-                    <UpdateDialog updateField={"username"} action={updateName}/>
-                </div>
-                <div className="flex flex-row">
-                    <p>Address: {user?.address}</p>
-                    <UpdateDialog updateField={"address"} action={updateAddress}/>
+            <div className="row-span-3 flex justify-center">
+                <div className="h-52 w-3/5 mt-20 p-2 bg-slate-300 rounded-2xl">
+                    <div className="flex flex-col items-center mb-1">
+                        <Avatar className="h-20 w-20">
+                            <AvatarImage src="" />
+                            <AvatarFallback className="bg-transparent">
+                                <AccountCircleIcon className="h-full w-full" />
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-row gap-1 justify-between">
+                            <p>{user?.name}</p>
+                            <UpdateDialog updateField={"username"} action={updateName}/>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-1 mb-1">
+                        <p>Email: {user?.email}</p>
+                        {/* <UpdateDialog updateField={"address"} action={updateAddress}/> */}
+                    </div>
+                    
+                    <div className="flex flex-row gap-1 mb-1">
+                        <p>Phone number: 0987654321</p>
+                        <UpdateDialog updateField={"address"} action={updateAddress}/>
+                    </div>
+
+                    <div className="flex flex-row gap-1 mb-1">
+                        <p>Address: {user?.address}</p>
+                        <UpdateDialog updateField={"address"} action={updateAddress}/>
+                    </div>
                 </div>
             </div>
-            <div className="col-span-2 bg-sky-400">
-                <p>wishlist</p>
+            <div className="col-span-2">
+                <div className="max-h-60 overflow-y-auto">
+                    <table className="w-full border-hidden">
+                        <thead className="sticky top-0">
+                            <tr>
+                                <th>ID</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>This is ID</td>
+                                <td>This is status</td>
+                            </tr>
+
+                            <tr>
+                                <td>This is ID</td>
+                                <td>This is status</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div className="col-span-2 bg-red-600">
-                <p>trip</p>
+            <div className="col-span-2">
+                <div className="max-h-60 overflow-y-auto">
+                    <table className="w-full border-hidden">
+                        <thead className="sticky top-0">
+                            <tr>
+                                <th>ID</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>This is ID</td>
+                                <td>This is status</td>
+                            </tr>
+
+                            <tr>
+                                <td>This is ID</td>
+                                <td>This is status</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </main>
     )
