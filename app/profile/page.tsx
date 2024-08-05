42eeef7f448771502dc572a0d41ca027f21cdf64
Tuan Le <leanhtuan030523@gmail.com>
2024-08-05 16:06:59 +0700

    Change layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,6 +5,11 @@ import { getAuthenticatedUserData } from "@/utils/cookie";
 import { revalidatePath } from "next/cache";
 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MapIcon from '@mui/icons-material/Map';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Link from "next/link";
 
 export default async function ProfilePage(){
     const userFromToken = await getAuthenticatedUserData();
@@ -25,85 +30,67 @@ export default async function ProfilePage(){
 
     return (
         <main
-            className="h-screen grid grid-rows-3 grid-flow-col gap-4"
+            className="h-screen"
         >
-            <div className="row-span-3 flex justify-center">
-                <div className="h-52 w-3/5 mt-20 p-2 bg-slate-300 rounded-2xl">
-                    <div className="flex flex-col items-center mb-1">
+            <div className="flex flex-col items-center gap-3">
+                <div className="flex h-52 w-2/5 mt-10 p-2 bg-slate-300 rounded-2xl">
+                    <div className="flex flex-col justify-center items-center w-1/4">
                         <Avatar className="h-20 w-20">
                             <AvatarImage src="" />
                             <AvatarFallback className="bg-transparent">
                                 <AccountCircleIcon sx={{fontSize: 80}}/>
                             </AvatarFallback>
                         </Avatar>
-                        <div className="flex flex-row gap-1 justify-between">
-                            <p>{user?.name}</p>
+                    </div>
+
+                    <div className="flex flex-col justify-center w-4/5">
+                        <div className="flex flex-row mb-1 justify-between">
+                            <p>Name: {user?.name}</p>
                             <UpdateDialog updateField={"username"} action={updateName}/>
                         </div>
-                    </div>
-                    <div className="flex flex-row gap-1 mb-1">
-                        <p>Email: {user?.email}</p>
-                        {/* <UpdateDialog updateField={"address"} action={updateAddress}/> */}
-                    </div>
-                    
-                    <div className="flex flex-row gap-1 mb-1">
-                        <p>Phone number: 0987654321</p>
-                        <UpdateDialog updateField={"address"} action={updateAddress}/>
-                    </div>
+                        <div className="flex flex-row mb-1 justify-between">
+                            <p>Email: {user?.email}</p>
+                            {/* <UpdateDialog updateField={"address"} action={updateAddress}/> */}
+                        </div>
+                        <div className="flex flex-row mb-1 justify-between">
+                            <p>Phone number: 0987654321</p>
+                            <UpdateDialog updateField={"address"} action={updateAddress}/>
+                        </div>
 
-                    <div className="flex flex-row gap-1 mb-1">
-                        <p>Address: {user?.address}</p>
-                        <UpdateDialog updateField={"address"} action={updateAddress}/>
+                        <div className="flex flex-row mb-1 justify-between">
+                            <p>Address: {user?.address}</p>
+                            <UpdateDialog updateField={"address"} action={updateAddress}/>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-span-2">
-                <div className="max-h-60 overflow-y-auto">
-                    <table className="w-full border-hidden">
-                        <thead className="sticky top-0">
-                            <tr>
-                                <th>ID</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>This is ID</td>
-                                <td>This is status</td>
-                            </tr>
-
-                            <tr>
-                                <td>This is ID</td>
-                                <td>This is status</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
+            
+                <div className="w-2/5 grid grid-cols-3 gap-5">
+                    <Link href={"/map"}>
+                         <button className="w-48 h-48 border border-black rounded-xl">
+                            <MapIcon sx={{fontSize: 70}} />
+                            <p>Map</p>
+                        </button>
+                    </Link>
+                    <Link href={"/trips"}>
+                        <button className="w-48 h-48 border border-black rounded-xl">
+                            <ShoppingCartIcon sx={{ fontSize: 70 }} />
+                            <p>Trips</p>
+                        </button>
+                    </Link>
+                    <Link href={"/wishlists"}>
+                        <button className="w-48 h-48 border border-black rounded-xl">
+                            <AssignmentIcon sx={{ fontSize: 70 }} />
+                            <p>Wishlists</p>
+                        </button>
+                    </Link>
+                    <div>
+                        <button className="w-48 h-48 border border-black rounded-xl">
+                            <LogoutIcon sx={{ fontSize: 70 }} />
+                            <p>Logout</p>
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className="col-span-2">
-                <div className="max-h-60 overflow-y-auto">
-                    <table className="w-full border-hidden">
-                        <thead className="sticky top-0">
-                            <tr>
-                                <th>ID</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>This is ID</td>
-                                <td>This is status</td>
-                            </tr>
-
-                            <tr>
-                                <td>This is ID</td>
-                                <td>This is status</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
+                
             </div>
         </main>
     )
