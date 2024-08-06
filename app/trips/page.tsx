efda97fc21d25e9d1338c347fb602d41b7b606e2
Tuan Le <leanhtuan030523@gmail.com>
2024-08-06 15:36:18 +0700

    Add Link go back to profile page

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -5,6 +5,8 @@ import { format } from "date-fns";
 import { eq } from "drizzle-orm";
 import Link from "next/link";
 
+import ArrowBackIosNew from "@mui/icons-material/ArrowBackIosNew";
+
 async function TripsPage() {
     const userData = await getAuthenticatedUserData();
 
@@ -22,45 +24,53 @@ async function TripsPage() {
 
     return (
         <main
-            className="h-screen mx-24 mt-10"
+            className="relative h-screen p-1"
         >
-            <h1 className="text-3xl font-medium mb-2">Your Trips</h1>
-            <table
-                className="max-h-4/5 border-collapse border border-4 border-slate-500"
-            >
-                <thead>
-                    <tr className="bg-slate-300">
-                        <th className="text-left border border-slate-700">ID</th>
-                        <th className="text-left border border-slate-700">Supermarket</th>
-                        <th className="text-left border border-slate-700">Owner</th>
-                        <th className="text-left border border-slate-700">Status</th>
-                        <th className="text-left border border-slate-700">Accepted at</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {trips.map((trip, index) => (
-                        <tr key={index}>
-                            <td className="py-2 pr-2 text-left border border-slate-700">
-                                <Link href={`/trips/${trip.id}`}>                                
-                                    {trip.id} 
-                                </Link>
-                            </td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">
-                                {trip.supermarket} 
-                            </td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">
-                                {trip.owner} 
-                            </td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">
-                                {trip.status} 
-                            </td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">
-                                {format(trip.updatedAt as Date, "dd/MM/yyyy")} 
-                            </td>
+            <div className="absolute">
+                <Link href={"/profile"} className="hover:underline">
+                    <ArrowBackIosNew fontSize="small" />
+                    Profile
+                </Link>
+            </div>
+            <div className="mx-24">
+                <h1 className="text-3xl font-medium mb-2">Your Trips</h1>
+                <table
+                    className="max-h-4/5 border-collapse border border-4 border-slate-500"
+                >
+                    <thead>
+                        <tr className="bg-slate-300">
+                            <th className="text-left border border-slate-700">ID</th>
+                            <th className="text-left border border-slate-700">Supermarket</th>
+                            <th className="text-left border border-slate-700">Owner</th>
+                            <th className="text-left border border-slate-700">Status</th>
+                            <th className="text-left border border-slate-700">Accepted at</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {trips.map((trip, index) => (
+                            <tr key={index}>
+                                <td className="py-2 pr-2 text-left border border-slate-700">
+                                    <Link href={`/trips/${trip.id}`}>                                
+                                        {trip.id} 
+                                    </Link>
+                                </td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">
+                                    {trip.supermarket} 
+                                </td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">
+                                    {trip.owner} 
+                                </td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">
+                                    {trip.status} 
+                                </td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">
+                                    {format(trip.updatedAt as Date, "dd/MM/yyyy")} 
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </main>
     )
 }
--- app/wishlists/page.tsx
+++ app/wishlists/page.tsx
@@ -3,43 +3,53 @@ import { getAuthenticatedUserData } from "@/utils/cookie";
 import { format } from "date-fns";
 import Link from "next/link";
 
+import ArrowBackIosNew from "@mui/icons-material/ArrowBackIosNew";
+
 async function WishlistsPage() {
     const userData = await getAuthenticatedUserData();
     const wishlists = await getUserWishlists(userData?.id as string);
 
     return (
         <main
-            className="h-screen mx-24 mt-10"
+            className="relative h-screen p-1"
         >
-            <h1 className="text-3xl font-medium mb-2">Your Wishlists</h1>
-            <table
-                className="max-h-4/5 border-collapse border border-4 border-slate-500"
-            >
-                <thead>
-                    <tr className="bg-slate-300">
-                        <th className="text-left border border-slate-700">ID</th>
-                        <th className="text-left border border-slate-700">Supermarket</th>
-                        <th className="text-left border border-slate-700">Created at</th>
-                        <th className="text-left border border-slate-700">Updated at</th>
-                        <th className="text-left border border-slate-700">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {wishlists.map((wishlist, index) => (
-                        <tr key={index}>
-                            <td className="py-2 pr-2 text-left border border-slate-700">
-                                <Link href={`/wishlists/${wishlist.id}`}>                                
-                                    {wishlist.id}
-                                </Link>
-                            </td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">{wishlist.supermarket}</td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">{format(wishlist.createdAt as Date, "dd/MM/yyyy")}</td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">{format(wishlist.updatedAt as Date, "dd/MM/yyyy")}</td>
-                            <td className="py-2 pr-2 text-left border border-slate-700">{wishlist.status}</td>
+            <div className="absolute">
+                <Link href={"/profile"} className="hover:underline">
+                    <ArrowBackIosNew fontSize="small" />
+                    Profile
+                </Link>
+            </div>
+            <div className="mx-24">
+                <h1 className="text-3xl font-medium mb-2">Your Wishlists</h1>
+                <table
+                    className="max-h-4/5 border-collapse border border-4 border-slate-500"
+                >
+                    <thead>
+                        <tr className="bg-slate-300">
+                            <th className="text-left border border-slate-700">ID</th>
+                            <th className="text-left border border-slate-700">Supermarket</th>
+                            <th className="text-left border border-slate-700">Created at</th>
+                            <th className="text-left border border-slate-700">Updated at</th>
+                            <th className="text-left border border-slate-700">Status</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {wishlists.map((wishlist, index) => (
+                            <tr key={index}>
+                                <td className="py-2 pr-2 text-left border border-slate-700">
+                                    <Link href={`/wishlists/${wishlist.id}`}>                                
+                                        {wishlist.id}
+                                    </Link>
+                                </td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">{wishlist.supermarket}</td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">{format(wishlist.createdAt as Date, "dd/MM/yyyy")}</td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">{format(wishlist.updatedAt as Date, "dd/MM/yyyy")}</td>
+                                <td className="py-2 pr-2 text-left border border-slate-700">{wishlist.status}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </main>
     )
 }
